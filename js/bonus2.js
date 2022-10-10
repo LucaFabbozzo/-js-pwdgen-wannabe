5fc1930cdb94d3ea2277c3e4d269b27abaf12488
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-10 08:40:56 -0500

    [add] bonus2

---
+++ js/bonus2.js
@@ -0,0 +1,21 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito,
+// poi chiedi il suo anno di nascita,
+// Infine scrivi sulla pagina nomecognomecolorepreferito
+// seguito da età dell'utente calcolata
+
+
+
+// const annoAttuale = 2022;
+
+// const nome = prompt('Come ti chiami?');
+// const cognome = prompt('Quale è il tuo cognome?');
+// const colorePreferito = prompt('Dimmi il tuo colore preferito?');
+// const annoDiNascita = prompt('In che anno sei nato?');
+// const miaEta = annoAttuale - annoDiNascita;
+
+
+// document.getElementById('age').innerHTML = `
+//   ${nome}${cognome}${colorePreferito}${miaEta}
+// `;
