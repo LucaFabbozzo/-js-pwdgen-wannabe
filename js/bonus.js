a606738de32365144d6d44c7091bc8cf3d404226
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-10 08:29:22 -0500

    [add] bonus

---
+++ js/bonus.js
@@ -0,0 +1,16 @@
+// Chiedi all'utente il suo nome,
+// poi chiedi il suo anno di nascita,
+// calcola la sua età
+// Infine scrivi sulla pagina l'utente ha (età utente calcolata)
+
+
+// const annoAttuale = 2022;
+
+
+// const nome = prompt('Come ti chiami?');
+// const anno = prompt('In che anno sei nato?');
+// const etaUtente = annoAttuale - anno;
+
+// document.getElementById('age').innerHTML = `
+//   ${nome} ha ${etaUtente} anni.
+// `;
