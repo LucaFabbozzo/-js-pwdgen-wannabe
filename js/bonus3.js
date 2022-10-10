ad22cf2880c33db82e311c312a15faca08fcf324
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-10 09:10:11 -0500

    [add] bonus3

---
+++ js/bonus3.js
@@ -0,0 +1,20 @@
+// Ho un prodotto che costa 8 euro, chiedo all'utente quanti ne vuole comprare, gli sommo il 22% di iva e gli faccio il 50% di sconto. Nell'output inserisco prezzo prodotto, quanti ne ho acquistati, prezzo intero, prezzo scontato. 
+
+// const prezzoProdotto = 8;
+// const iva = 0.22;
+// const sconto = 0.5; 
+// const prodottiRichiesti = parseInt(prompt('Quanti ne vuoi acquistare?'));
+
+
+// const prezzoIntero = prodottiRichiesti * prezzoProdotto;
+// const calcoloIva = prezzoIntero * iva;
+// const prezzoConIva = prezzoIntero + calcoloIva;
+// const prezzoScontato = prezzoConIva * sconto;
+
+
+// document.getElementById('prize').innerHTML = `
+//   Prezzo prodotto = ${prezzoProdotto} , 
+//   Prodotti Aquistati = ${prodottiRichiesti} ,
+//   Prezzo Intero = ${prezzoIntero} ,
+//   Prezzo Scontato = ${prezzoScontato}
+// `;
