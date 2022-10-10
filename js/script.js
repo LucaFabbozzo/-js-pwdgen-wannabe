e954c977a6e7eeb864ba36e419fa54abc8eb68ab
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-10 08:04:20 -0500

    Last

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('hello world!')
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito22
+
+
+const nome = prompt('Come ti chiami?');
+const cognome = prompt('Quale è il tuo cognome?');
+const colorePreferito = prompt('Dimmi il tuo colore preferito?')
+
+
+document.getElementById('password').innerHTML = `
+  ${nome}${colorePreferito}22.
+`;
